17f3d406942d9d0ffaea1035b813fbf916962da5
bogomazov-a <bogomazov-a@gaz-is.ru>
2019-12-02 16:13:42 +0300

    [v02] - Update to Vue

---
+++ resources/javascript/main.js
@@ -0,0 +1,12 @@
+let app = new Vue({
+   el: '#app',
+   data: {
+       console_log: 'datas.getJSON',
+       altText: 'Hello',
+       image: './image.jpeg',
+       joke: "hello world",
+       commands: ["help","about","projects","expirience","contacts","funfact"],
+       hidden_link: 'matrix.html',
+       cv_link: "resources/cv.pdf"
+   }
+})
